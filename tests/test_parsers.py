e58c3a7e5c2ff2e00490e87bce98bd934c83512d
Noah Hoffman <noah.hoffman@gmail.com>
2011-11-10 22:51:46 -0800

    stubbed out unit tests

---
+++ tests/test_parsers.py
@@ -0,0 +1,20 @@
+import os
+import unittest
+import logging
+import pprint
+
+import __init__ as config
+
+log = logging.getLogger(__name__)
+
+class TestCast(unittest.TestCase):
+
+    def setUp(self):
+        self.funcname = '_'.join(self.id().split('.')[-2:])
+
+    def tearDown(self):
+        pass
+
+    def test01(self):
+        print 'hi'
+        
