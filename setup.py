cbf8a794813bc38ce2d37f521a26bab0a6638e28
Noah Hoffman <noah.hoffman@gmail.com>
2011-11-10 22:09:05 -0800

    installs package data

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 Create unix package:    python setup.py sdist
 """
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    import distribute_setup
-    distribute_setup.use_setuptools()
-    from setuptools import setup, find_packages
+from distutils.core import setup
 
-import glob
+# try:
+#     from setuptools import setup, find_packages
+# except ImportError:
+#     import distribute_setup
+#     distribute_setup.use_setuptools()
+#     from setuptools import setup, find_packages
 
 from opiate import __version__
 
@@ -17,7 +17,8 @@ params = {'author': 'Noah Hoffman',
           'author_email': 'ngh2@uw.edu',
           'description': 'Analysis of LC/MS opiates assay',
           'name': 'opiate',
-          'packages': find_packages(exclude=['tests']),
+          'packages': ['opiate','opiate.scripts','opiate.subcommands'],
+          'package_dir': {'opiate': 'opiate'},
           'scripts': ['smack'],
           'version': __version__,
           # 'install_requires': ['sqlalchemy', 'decorator'],
