8f7f1b69e6abc7a1c4250bdb20901be8adde7676
Noah Hoffman <noah.hoffman@gmail.com>
2011-11-16 13:21:55 -0800

    qa display in development

--- opiate/display.py
+++ opiate/display.py
@@ -1,5 +1,7 @@
 from itertools import chain, groupby
 
+import pprint
+
 from opiate import CONTROL_NAMES
 from opiate.calculations import all_checks
 calc_names = dict((name, d['description']) for name, d in all_checks.items())
@@ -21,6 +23,11 @@ def display_qa_results(results, show_all = False):
 
     show = (lambda x: True) if show_all else (lambda x: x is False)
 
+    # sort results by compound then by sample
+    results = sorted(list(results), key = lambda r: (r[0].COMPOUND_id, r[0].SAMPLE_id))
+    pprint.pprint(results[1:20])
+    sys.exit()
+    
     for sample_id, sample_group in groupby(results, lambda r: r[0].SAMPLE_id):
         print 'Sample', sample_id, controls.get(sample_id, '')
         for cmpnd_id_and_name, compound_group in groupby(sample_group, lambda r: (r[0].COMPOUND_id, r[0].COMPOUND_name)):
