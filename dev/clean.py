05e3c9f3d748bc3c7f7ac632773093f51720365e
Noah Hoffman <noah.hoffman@gmail.com>
2011-11-26 23:05:32 -0800

    clean and smudge don't screw up newlines

--- dev/clean.py
+++ dev/clean.py
@@ -2,11 +2,12 @@
 
 import fileinput
 from os import path
+import sys
 
-for line in fileinput.input([path.join('opiate','__init__.py')], inplace = True, backup = '.bak'):
+for line in fileinput.input([path.join('opiate','__init__.py')], inplace = True):
     if line.startswith('_sha ='):
-        line = "_sha = ''"
-    print line
+        line = "_sha = ''\n"
+    sys.stdout.write(line)
 
 
 
--- dev/smudge.py
+++ dev/smudge.py
@@ -3,13 +3,13 @@
 import subprocess
 import fileinput
 from os import path
+import sys
 
 sha = subprocess.check_output(
     args = ['git', '--no-pager', 'log', '--pretty=format:"%h"', '-1']
     ).strip('"')
 
-for line in fileinput.input([path.join('opiate','__init__.py')], inplace = True, backup = '.bak'):
+for line in fileinput.input([path.join('opiate','__init__.py')], inplace = True):
     if line.startswith('_sha ='):
-        line = "_sha = '.%s'" % sha
-    print line
-
+        line = "_sha = '.%s'\n" % sha
+    sys.stdout.write(line)
