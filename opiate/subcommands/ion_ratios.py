f8158d76ba88c14e3b59403284c18f994ce6599c
Noah Hoffman <noah.hoffman@gmail.com>
2011-11-26 16:50:20 -0800

    add opiate/subcommands/ion_ratios.py

---
+++ opiate/subcommands/ion_ratios.py
@@ -0,0 +1,57 @@
+"""Calculate average Ion Ratios for each compound."""
+
+from collections import OrderedDict
+from itertools import chain, ifilter
+from os import path
+
+import argparse
+import csv
+import json
+import logging
+import sys
+import pprint
+
+from opiate import matrix_file, qafile, CONTROL_NAMES
+from opiate.parsers import qa_from_csv, read_matrix, group_samples
+from opiate.display import display_specimens, display_controls
+from opiate.calculations import mean_ion_ratios
+
+log = logging.getLogger(__name__)
+
+def build_parser(parser):
+    parser.add_argument('infile', help='Input xml or json file containing experimental data.')
+    parser.add_argument('-o','--outfile', metavar = 'FILE', default = sys.stdout,
+                        type = argparse.FileType('w'))
+    
+def action(args):
+    nullchar = '.' if args.outfile == sys.stdout else ''
+    fmt = lambda s: '%.2f' % s if isinstance(s, float) else (s or nullchar)
+
+    qadata = qa_from_csv(qafile)
+
+    if args.infile.lower().endswith('.xml'):
+        controls, sample_groups = group_samples(args.infile)
+    else:
+        with open(args.infile) as f:
+            controls, sample_groups = json.load(f)        
+
+    std_ids, std_names = zip(
+        *[(i,n) for i,n in CONTROL_NAMES if n.startswith('std')])
+
+    headers = ['qa_id', 'qa_compound'] + \
+        list(std_names) + ['ion_ratio_average_calc','ion_ratio_average']
+    writer = csv.DictWriter(args.outfile,
+                            fieldnames = headers, extrasaction = 'ignore')
+
+    ion_ratios = mean_ion_ratios(controls, set(std_ids))    
+    for compound_id, d in ion_ratios.items():
+        if compound_id == min(ion_ratios.keys()):
+            writer.writerow(dict(zip(headers, headers)))
+            
+        d.update(qadata[compound_id])
+        writer.writerow(dict((k, fmt(d.get(k))) for k in headers))
+
+        
+if __name__ == '__main__':
+    main()
+    
--- opiate/subcommands/qa.py
+++ opiate/subcommands/qa.py
@@ -14,12 +14,13 @@ import xml.etree.ElementTree
 from opiate import matrix_file, qafile
 from opiate.parsers import qa_from_csv, read_matrix, group_samples
 from opiate.display import display_specimens, display_controls
-from opiate.containers import Compound, flatten
+from opiate.containers import Compound
+from opiate.utils import flatten
 
 log = logging.getLogger(__name__)
 
 def build_parser(parser):
-    parser.add_argument('infile', help='XML file')
+    parser.add_argument('infile', help='Input xml or json file containing experimental data.')
     parser.add_argument('-o','--outfile', metavar = 'FILE', default = sys.stdout,
                         type = argparse.FileType('w'))
     parser.add_argument('-a','--show-all', help='Show all results for each compound (ie, not just QA failures)',
