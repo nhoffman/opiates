af8477aaf4f134567e0084eb2a9f5098c4c5c2a8
Noah Hoffman <noah.hoffman@gmail.com>
2012-01-06 17:17:12 -0800

    start to implement qa calculations for results

--- opiate/containers.py
+++ opiate/containers.py
@@ -238,6 +238,10 @@ class Sample(object):
         self.qa_ok = all(c.qa_ok for c in compounds)
         self.abbrev_name = (self.COMPOUND_name[:10] + '...') if len(self.COMPOUND_name) > 10 else self.COMPOUND_name
 
+        # these abbreviations are used in several calculations
+        self.a = self.compounds['straight10']
+        self.c = self.compounds['straight']
+        
     def __repr__(self):
         return '<Sample %(sample_label)s %(abbrev_name)s (%(type)s)>' % \
             self
@@ -247,7 +251,53 @@ class Sample(object):
 
     def get(self, key, default = None):
         return self.__dict__.get(key, default)
+        
+    def check_is_peak_area(self):
+        """
+        I.S. Peak Area: If IS Pk Area fails in c, but doesn't fail for
+        a (x10), then it passes. If a also fails, check recovery of d
+        (need to talk to Andy about exact requirements).
 
+        Confirm with Jane: this seems to suggest that all we need to
+        check is whether a passes.
+        """
+        
+        return self.a.qa_results['is_peak_area'] is not False
+
+    def check_amr(self):
+        """
+        AMR: If peak_analconc < amr_low, report NRN.  If peak_analconc
+        > amr_high, report from a (x10).  Do not report QA fail when
+        this is flagged. Essentially, this is ignored in the results
+        and Jane will remove from the matrix for all but glucuronide
+        spiked compounds (b,d).
+        """
+
+        # not sure what to do with this yet
+        return True
+
+    def check_ion_ratio(self):
+        """
+        Ratio: For c, if ion ratio fails when peak_analconc < amr_low,
+        report NRN (not QA failure). If ratio fails when peak_analconc
+        > amr_low, check a.  If ion ratio corrects in a, report result
+        from a.  If it does not correct, report QA fail.
+
+        Again, looks like we just need to make sure that a passes.
+        """
+
+        pass
+
+    def check_rrt(self):
+        """
+        RRT: If RRT fails when peak_analconc < amr_low, report NRN
+        (not QA failure). If RRT fails when peak_analconc > amr_low,
+        check a.  If RRT corrects in a, report result from a.  If it
+        does not correct, report QA fail.
+        """
+
+        pass
+    
     def result(self, pretty = False, nullchar = ''):
         """
         If c > amr_high, calculate the result from a by multiplying by 10
@@ -256,6 +306,15 @@ class Sample(object):
 
         Use whole numbers, except for fentanyl (compound_id 11) use 2
         decimal places.
+
+        TODO: The QA report is ordering the specimens in alphabetical
+        order instead of the order in which they were run - can we
+        change this?  It would be easier to follow if it matched the
+        worklist.
+        fix by ordering by specimen id.
+
+        TODO: show label for specimen c in results.
+
         """
 
         conc = lambda x: (x.PEAK_analconc or 0)
@@ -271,8 +330,14 @@ class Sample(object):
                 val = nullchar if pretty else 'NRN'
             else:
                 val = 'POS'
+
+        # need to look for QA failures for the individual compounds
+        # used for the result
         else:
             if conc(c) < low:
+                # first look at IS Peak area before we know if neg
+                # result is ok. If this qa calc passes in a, then it's
+                # ok to report neg, otherwise, qa_fail
                 val = nullchar if pretty else ('<%s' % low)
             elif low <= conc(c) <= high:
                 val = fmt % conc(c)
